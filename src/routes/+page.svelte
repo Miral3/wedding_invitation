b5af0fee4730c72fb057a07a684f015a6980584e
yongsangyou <yongsang.you@ascentnet.co.jp>
2025-04-21 16:01:24 +0900

    초기설정

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>김철수, 김영희 결혼합니다♥</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section></section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+</style>
